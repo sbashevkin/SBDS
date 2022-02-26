663ac1fd6cc38c2306bf640f5dac623a60592f71
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2022-02-25 16:33:28 -0800

    Updating to include livneh data

--- anthropocene figure.R
+++ anthropocene figure.R
@@ -1,6 +1,6 @@
 library(dplyr)
 library(lubridate)
-library(readxl)
+library(readr)
 library(ggplot2)
 
 temp_data<-read_csv("Delta_Yolo_water_temp_mean.csv")%>%
@@ -9,22 +9,31 @@ temp_data<-read_csv("Delta_Yolo_water_temp_mean.csv")%>%
 
 temp_data_25A<-temp_data%>%
   group_by(`Station Name`, Year)%>%
-  summarise(Days_over_25=length(which(GFDL.CM3_rcp45>25)), .groups="drop")%>%
+  summarise(modeled_days_over_25=length(which(GFDL.CM3_rcp45>25)),
+            measured_days_over_25=length(which(livneh_historical>25)), .groups="drop")%>%
   group_by(Year)%>%
-  summarise(Days_over_25=mean(Days_over_25))%>%
-  mutate(Calculation="Calculate days over 25, then average across stations")
+  summarise(across(c(modeled_days_over_25, measured_days_over_25), mean))%>%
+  mutate(Calculation="Calculate days over 25, then average across stations",
+         measured_days_over_25=if_else(Year>=2014, NA_real_, measured_days_over_25))
 
 temp_data_25B<-temp_data%>%
   group_by(Date, Year)%>%
-  summarise(GFDL.CM3_rcp45=mean(GFDL.CM3_rcp45), .groups="drop")%>%
+  summarise(across(c(GFDL.CM3_rcp45, livneh_historical), mean), .groups="drop")%>%
   group_by(Year)%>%
-  summarise(Days_over_25=length(which(GFDL.CM3_rcp45>25)), .groups="drop")%>%
-  mutate(Calculation="Average across stations, then calculate days over 25")
+  summarise(modeled_days_over_25=length(which(GFDL.CM3_rcp45>25)),
+            measured_days_over_25=length(which(livneh_historical>25)), .groups="drop")%>%
+  mutate(Calculation="Average across stations, then calculate days over 25",
+         measured_days_over_25=if_else(Year>=2014, NA_integer_, measured_days_over_25))
 
 combined<-temp_data_25A%>%
   bind_rows(temp_data_25B)
 
-ggplot(combined, aes(x=Year, y=Days_over_25, color=Calculation))+
+ggplot(combined, aes(x=Year, y=modeled_days_over_25, color=Calculation))+
+  geom_line()+
+  theme_bw()+
+  theme(legend.position="bottom")
+
+ggplot(combined, aes(x=Year, y=measured_days_over_25, color=Calculation))+
   geom_line()+
   theme_bw()+
   theme(legend.position="bottom")
